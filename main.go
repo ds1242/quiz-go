58bb21dca0b28b51223b20f879ac56929a15e7b7
David Shaw <dshaw11@utk.edu>
2024-06-20 15:00:51 -0400

    read from local generic csv file

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	// "errors"
+	"os"
+)
 
 func main()  {
-	fmt.Println("hello world")
+	questionMap := make(map[string]string)
+
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = 2
+	data, err := reader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, row := range data {
+		questionMap[row[0]] = row[1]
+	}
+
+	for key, val := range questionMap {
+		fmt.Printf("%s = %s\n", key, val)
+	}
 }
