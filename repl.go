a605a4b0e23c7e598de996c3dff76cfed12a1061
David Shaw <dshaw11@utk.edu>
2024-06-20 16:00:13 -0400

    created function to clean user input

--- repl.go
+++ repl.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-	// "bufio"
-	// "os"
-	// "strings"
+	"bufio"
+	"os"
+	"strings"
 )
 
 
@@ -12,4 +12,11 @@ func startRepl(quiz *Quiz) {
 	for key, val := range quiz.QuestionMap {
 		fmt.Printf("%s = %s\n", key, val)
 	}
+}
+
+
+func cleanInput(text string) []string {
+	output := strings.ToLower(text)
+	words := strings.Fields(output)
+	return words
 }
